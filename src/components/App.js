b360bb28c66079994bf8f4e057a13b20f43e3e4a
pepe lopez <plopez741@gmail.com>
2022-05-17 20:07:47 -0700

    added sigup componment

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Signup from './login/signup/logout/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Signup />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/signup/logout/Signup.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+
+
+
+function Signup(){
+    
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+    //handles the user creation
+    function submitUser(e){
+        e.preventDefault()
+        fetch("http://localhost:3000/user",{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                email,
+                password,
+                password_confirmation: passwordConfirmation,
+            }),
+        })
+            .then(r => r.json())
+            .then(data => data.errors ? alert(data.errors) :console.log(data))
+    }
+
+    return(
+        <>
+        <form onSubmit={(e) => submitUser(e)} >
+                <label>Username:
+                    <input
+                        type="text"
+                        placeholder="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <label>Email:
+                    <input
+                        type="text"
+                        placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label>Password:
+                    <input
+                        type="text"
+                        placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <label>Re-Enter Password:
+                    <input
+                        type="text"
+                        placeholder="Re-Enter Password"
+                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                    />
+                </label>
+
+                <input type="submit" className="submitButton" />
+                
+            </form>
+        </>
+    )
+}
+
+
+export default Signup;
